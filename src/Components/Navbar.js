141d41c4e5cc54230038318a0dfa004fd5c4c0a3
gifin-mj <gifinmj@gmail.com>
2022-06-06 11:01:13 +0530

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+import logo from '../Images/Vector.png'
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <img src={logo} alt='fvdc'/>
+        </div>
+  )
+}
+
+export default Navbar
